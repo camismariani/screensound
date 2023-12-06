449b12feb76a2338ea282b9ebd92e8306a6ff73e
Camila <camilamariani.cm@gmail.com>
2023-12-05 22:15:41 -0300

    Configurando menu de opções e exibindo mensagem de boas-vindas

---
+++ screen_sound_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string mensagemDeBoasVindas = "Boas vindas ao screen sound";
+
+
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine("**************************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("**************************");
+
+}
+
+void ExibiOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite sua opção: ");
+    Console.ReadLine();
+}
+
+
+ExibirMensagemDeBoasVindas();
+ExibiOpcoesDoMenu();
